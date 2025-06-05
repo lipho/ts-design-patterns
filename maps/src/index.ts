2ec46875c78b65ca044c374ff487bccba6a8922a
lipho <hello@lipho.net>
2025-06-04 23:16:42 -0700

    load gmaps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,11 @@
 import {User} from "./User";
 import {Company} from "./Company";
 
-console.log(new User());
-console.log(new Company());
+
+new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 0, lng: 0},
+    zoom: 1
+});
+
+// console.log(new User());
+// console.log(new Company());
